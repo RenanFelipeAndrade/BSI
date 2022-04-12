1837b516a8eb075a2eeb9a1d230faf11ceb0b639
Renan Andrade <Fel9.renan02@gmail.com>
2022-04-12 08:11:40 -0300

    Corrigido erro de digitação

--- Lista de exercícios 1 - variáveis e operadores/prg_mediaFinalAprovadoReprovado.py
+++ Lista de exercícios 1 - variáveis e operadores/prg_mediaFinalAprovadoReprovado.py
@@ -3,7 +3,7 @@ second_test_grade = float(input("Insira a nota da segunda prova: "))
 first_exercise_grade = float(input("Insira a nota do primeiro exercício: "))
 second_exercise_grade = float(input("Insira a nota do segundo exercício: "))
 
-# weighted average: test weights 7; exercise weights 3; 20 is the sum of all wheights 7 + 7 + 3 + 3
+# weighted average: test weighs 7; 3 exercise weights; 20 is the sum of all weights 7 + 7 + 3 + 3
 status = "aprovado" if (first_test_grade * 7 + second_test_grade *
                         7 + first_exercise_grade * 3 + second_exercise_grade * 3) / 20 >= 7 else "reprovado"
 

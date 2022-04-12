ffc3ef3cf369ef10f6cf9e69f2604572013568ac
Renan Andrade <Fel9.renan02@gmail.com>
2022-04-12 08:08:36 -0300

    Terminado media final aprovado repovado

---
+++ Lista de exercícios 1 - variáveis e operadores/prg_mediaFinalAprovadoReprovado.py
@@ -0,0 +1,10 @@
+first_test_grade = float(input("Insira a nota da primeira prova: "))
+second_test_grade = float(input("Insira a nota da segunda prova: "))
+first_exercise_grade = float(input("Insira a nota do primeiro exercício: "))
+second_exercise_grade = float(input("Insira a nota do segundo exercício: "))
+
+# weighted average: test weights 7; exercise weights 3; 20 is the sum of all wheights 7 + 7 + 3 + 3
+status = "aprovado" if (first_test_grade * 7 + second_test_grade *
+                        7 + first_exercise_grade * 3 + second_exercise_grade * 3) / 20 >= 7 else "reprovado"
+
+print(f"O aluno foi {status}")

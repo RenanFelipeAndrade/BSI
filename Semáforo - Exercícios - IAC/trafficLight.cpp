827eda2ee450a80938fd607e3bac9254093baa42
Renan Andrade <Fel9.renan02@gmail.com>
2022-06-06 17:05:51 -0300

    Adicionado semáforo apenas com sensor

--- Semáforo - Exercícios - IAC/trafficLight.cpp
+++ Semáforo - Exercícios - IAC/trafficLight.cpp
@@ -9,134 +9,107 @@
 
 Adafruit_LiquidCrystal lcd_1(0);
 
-#define echoPin 5
-#define trigPin 4
+#define echoPin 7
+#define trigPin 6
 
 long duration;
 int distance;
 
-int firstGreen = 8;
-int firstYellow = 9;
-int firstRed = 10;
-int firstButton = 11;
+int firstGreen = 9;
+int firstYellow = 10;
+int firstRed = 11;
 
-int secondGreen = 0;
-int secondYellow = 1;
-int secondRed = 2;
-int secondButton = 3;
+int secondGreen = 2;
+int secondYellow = 3;
+int secondRed = 4;
+
+int shortDelay = 2500;
+int longDelay = 5000;
 
 void setup() {
+  // setup the sensor pins
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
+  // setup the first set of button + lights
   pinMode(firstGreen, OUTPUT);
   pinMode(firstYellow, OUTPUT);
   pinMode(firstRed, OUTPUT);
-  pinMode(firstButton, INPUT);
 
+  // setup the second set of button + lights
   pinMode(secondGreen, OUTPUT);
   pinMode(secondYellow, OUTPUT);
   pinMode(secondRed, OUTPUT);
-  pinMode(secondButton, INPUT);
 
-  digitalWrite(firstYellow, LOW);
-  digitalWrite(firstRed, LOW);
-
-  digitalWrite(secondYellow, LOW);
-  digitalWrite(secondGreen, LOW);
-
+  // initializes the lcd
   lcd_1.begin(16, 2);
 }
 
 void loop() {
-  // if (digitalRead(firstButton) == HIGH or digitalRead(secondButton) == HIGH)
-  // {
-  //   delay(15); // 15 miliseconds
+  int distance = sonarPulse();
 
-  //   if (digitalRead(firstButton) == HIGH or digitalRead(secondButton) ==
-  //   HIGH) {
-  //     // turn on firstRed
-  //     digitalWrite(firstRed, HIGH);
-  //     digitalWrite(firstYellow, LOW);
-  //     digitalWrite(firstGreen, LOW);
+  // 50 centimeters
+  if (distance <= 50) {
+    digitalWrite(firstRed, LOW);
+    digitalWrite(firstYellow, HIGH);
+    digitalWrite(firstGreen, LOW);
 
-  //     // turn on secondRed
-  //     digitalWrite(secondRed, HIGH);
-  //     digitalWrite(secondYellow, LOW);
-  //     digitalWrite(secondGreen, LOW);
+    digitalWrite(secondRed, HIGH);
+    digitalWrite(secondGreen, LOW);
+    digitalWrite(secondYellow, LOW);
+    delay(shortDelay);
 
-  //     for (int seconds = 0; seconds < 10; seconds++) {
-  //       lcd_1.clear();
-  //       lcd_1.print("SIGA");
-  //       lcd_1.setCursor(0, 1);
-  //       lcd_1.print(10 - seconds);
-  //       delay(1000);
-  //     }
-  //     digitalWrite(firstButton, LOW);
-  //     digitalWrite(secondButton, LOW);
-  //     digitalWrite(secondRed, LOW);
-  //     digitalWrite(firstRed, LOW);
-  //   }
-  // }
+    digitalWrite(firstRed, HIGH);
+    digitalWrite(firstYellow, LOW);
 
-  Serial.begin(9600);
-  Serial.println("Ultrasonic Sensor HC-SR04 Test");
-  Serial.println("with Arduino UNO R3");
+    // takes 10 seconds to complete the loop
+    for (int seconds = 0; seconds < 10; seconds++) {
+      lcd_1.clear();
+      lcd_1.print("SIGA");
+
+      lcd_1.setCursor(0, 1);
+      lcd_1.print(10 - seconds);
+      delay(1000); // 1 second
+    }
+    digitalWrite(firstRed, LOW);
+  }
+  trafficDefaultLoop();
+}
+
+void trafficDefaultLoop() {
+  lcd_1.clear();
+  lcd_1.print("PARE");
+
+  digitalWrite(firstGreen, HIGH);
+  digitalWrite(secondRed, HIGH);
+  delay(longDelay);
+
+  digitalWrite(firstGreen, LOW);
+  digitalWrite(secondRed, LOW);
+
+  digitalWrite(firstYellow, HIGH);
+  digitalWrite(secondGreen, HIGH);
+  delay(shortDelay);
+
+  digitalWrite(firstYellow, LOW);
+  digitalWrite(secondGreen, LOW);
+
+  digitalWrite(firstRed, HIGH);
+  digitalWrite(secondYellow, HIGH);
+  delay(longDelay);
+
+  digitalWrite(firstRed, LOW);
+  digitalWrite(secondYellow, LOW);
+
+  digitalWrite(firstGreen, HIGH);
+}
+
+int sonarPulse() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
-  if (distance <= 20) {
-
-    // turn on firstRed
-    digitalWrite(firstRed, HIGH);
-    digitalWrite(firstYellow, LOW);
-    digitalWrite(firstGreen, LOW);
-
-    // turn on secondRed
-    digitalWrite(secondRed, HIGH);
-    digitalWrite(secondYellow, LOW);
-    digitalWrite(secondGreen, LOW);
-
-    for (int seconds = 0; seconds < 10; seconds++) {
-      lcd_1.clear();
-      lcd_1.print("SIGA");
-      lcd_1.setCursor(0, 1);
-      lcd_1.print(10 - seconds);
-      delay(1000);
-    }
-    digitalWrite(firstButton, LOW);
-    digitalWrite(secondButton, LOW);
-    digitalWrite(secondRed, LOW);
-    digitalWrite(firstRed, LOW);
-  }
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  lcd_1.clear();
-  lcd_1.print("PARE");
-
-  digitalWrite(firstGreen, HIGH);
-  digitalWrite(secondRed, HIGH);
-  delay(2000); // 2 seconds
-
-  digitalWrite(firstGreen, LOW);
-  digitalWrite(secondRed, LOW);
-
-  digitalWrite(firstYellow, HIGH);
-  digitalWrite(secondYellow, HIGH);
-  delay(1000); // 1 second
-
-  digitalWrite(firstYellow, LOW);
-  digitalWrite(firstRed, HIGH);
-  digitalWrite(secondYellow, LOW);
-  digitalWrite(secondGreen, HIGH);
-  delay(2000); // 2 seconds
-
-  digitalWrite(firstRed, LOW);
-  digitalWrite(secondGreen, LOW);
+  return duration * 0.034 / 2;
 }

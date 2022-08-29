51b885ae0d9c012cb2dcafddd324bb95e369659f
Renan Andrade <Fel9.renan02@gmail.com>
2022-08-29 18:58:10 -0300

    Adicionado tema e dificuldades ao game

--- Python/Poetry/Embaralha palavra/index.py
+++ Python/Poetry/Embaralha palavra/index.py
@@ -1,21 +1,158 @@
 from random import shuffle, randint
 
 
-def sorteia_palavra():
-    palavras = [
-        "batata",
-        "jogo",
-        "tardigrado",
-        "oculos",
-        "quadro",
-        "computador",
-        "cabelo",
-        "arara",
-        "lasanha",
-    ]
-    shuffle(palavras)
-    palavra_sorteada = palavras[randint(0, len(palavras) - 1)]
-    return palavra_sorteada
+def sorteia_palavra(tema):
+    cidades = {
+        "f": [
+            "lages",
+            "criciúma",
+            "urubici",
+            "macaé",
+            "natal",
+            "cotia",
+            "majé",
+            "itú",
+            "sinóp",
+        ],
+        "m": [
+            "calcaia",
+            "taubaté",
+            "moçoró",
+            "sumaré",
+            "jaraguá",
+            "brusque",
+            "palhoça",
+            "curitiba",
+        ],
+        "d": [
+            "florianópolis",
+            "joinville",
+            "ituporanga",
+            "ressaquinha",
+            "petrópolis",
+            "uberlândia",
+            "ananindeua",
+            "carapicuíba",
+            "itaquaquecetuba",
+            "parnamirim",
+        ],
+    }
+
+    objetos = {
+        "f": [
+            "garfo",
+            "maiô",
+            "óculos",
+            "jeans",
+            "copo",
+            "harpa",
+            "funil",
+            "pote",
+            "faca",
+        ],
+        "m": [
+            "picareta",
+            "bigorna",
+            "pendulo",
+            "manomêtro",
+            "zabumba",
+            "notebook",
+            "pinico",
+            "cadeira",
+        ],
+        "d": [
+            "estetoscopio",
+            "zarabatana",
+            "quinticlave",
+            "sombrinha",
+            "nebulizador",
+            "namômetro",
+            "lantejoula",
+            "velocípede",
+        ],
+    }
+
+    paises = {
+        "f": [
+            "suíça",
+            "itália",
+            "chipre",
+            "suécia",
+            "rússia",
+            "china",
+            "chile",
+            "sudão",
+            "irã",
+        ],
+        "m": [
+            "monaco",
+            "austria",
+            "austrália",
+            "equador",
+            "noruega",
+            "albânia",
+            "romênia",
+            "georgia",
+            "turquia",
+            "iraque",
+        ],
+        "d": [
+            "eslovênia",
+            "somália",
+            "guatemala",
+            "nicarágua",
+            "filipinas",
+            "mogólia",
+            "cingapura",
+            "nigéria",
+            "camarões",
+            "uzbequistão",
+            "azerbaijão",
+        ],
+    }
+
+    verbos = {
+        "f": [
+            "falar",
+            "correr",
+            "pular",
+            "andar",
+            "sonhar",
+            "piscar",
+            "nadar",
+            "sentar",
+        ],
+        "m": [
+            "hibernar",
+            "trafegar",
+            "manipular",
+            "evacuar",
+            "capturar",
+            "caminhar",
+            "desfilar",
+            "reclamar",
+        ],
+        "d": [
+            "desfibrilar",
+            "tergivisar",
+            "perambular",
+            "interpelar",
+            "radicalizar",
+            "auscutar",
+            "impermebealizar",
+            "diagnosticar",
+        ],
+    }
+
+    temas = {1: cidades, 2: objetos, 3: paises, 4: verbos}
+
+    dificuldades = ["fácil", "médio", "difícil"]
+    dificuldade_sorteada = dificuldades[randint(0, len(dificuldades))]
+
+    lista_palavras = temas[tema][dificuldade_sorteada[0]]
+
+    palavra_sorteada = lista_palavras[randint(0, len(lista_palavras) - 1)]
+    return (palavra_sorteada, dificuldade_sorteada)
 
 
 def embaralhada_palavra(palavra, palavra_anterior=""):
@@ -42,15 +179,16 @@ def frases_motivacional():
     return frase_sorteada
 
 
-if __name__ == "__main__":
+def main(tema):
     tentativa = 0
-    palavra_sorteada = sorteia_palavra()
+    palavra_sorteada, dificuldade = sorteia_palavra(tema)
     palavra_embaralhada = embaralhada_palavra(palavra_sorteada)
 
     while True:
         frase_sorteada = frases_motivacional()
 
         print("A palavra embaralhada é", palavra_embaralhada)
+        print("A dificuldade é:", dificuldade)
         print("Você ganha se acerta em, no máximo, 5 tentativas")
 
         palpite = input("Qual é a palavra? \n> ")
@@ -65,3 +203,25 @@ if __name__ == "__main__":
             )
             break
         print(f"\n{frase_sorteada}")
+
+
+if __name__ == "__main__":
+    while True:
+        tema = int(
+            input(
+                """
+    Escolha um dos temas a seguir (digite o número do tema):
+        1 = cidade
+        2 = objetos
+        3 = paises
+        4 = verbos
+        
+        >> """
+            )
+        )
+        if tema > 4 or tema < 1:
+            print("--------- Escolha um tema válido! ---------")
+        else:
+            break
+
+    main(tema)

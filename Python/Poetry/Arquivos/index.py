a10e6e1f78692bd9cd77b8650818c34ddc78c638
Renan Andrade <Fel9.renan02@gmail.com>
2022-09-06 15:52:04 -0300

    Adicionado plotagem gráfico de barras comparativo

--- Python/Poetry/Arquivos/index.py
+++ Python/Poetry/Arquivos/index.py
@@ -1,13 +1,49 @@
 from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
+    texto = {}
     with open("./Python/Poetry/Arquivos/Richard_Dawkins_O_Gene_Egoista.txt") as arquivo:
-        texto = arquivo.read().lower()
-    letras = [character for character in texto if character.isalpha()]
-    frequencia_letras = Counter(letras)
+        texto["portugues"] = arquivo.read().lower()
+    with open(
+        "./Python/Poetry/Arquivos/Richard_Dawkins_O_Gene_Egoista_espanhol.txt"
+    ) as arquivo:
+        texto["espanhol"] = arquivo.read().lower()
 
-    print(frequencia_letras.most_common())
+    letras_portugues = [
+        character for character in texto["portugues"] if character.isalpha()
+    ]
+    letras_espanhol = [
+        character for character in texto["espanhol"] if character.isalpha()
+    ]
+
+    frequencia_letras = {}
+    frequencia_letras["portugues"] = Counter(letras_portugues)
+    frequencia_letras["espanhol"] = Counter(letras_espanhol)
+
+    rotulos_pt, valores_pt = zip(*frequencia_letras["portugues"].most_common(5))
+    rotulos_es, valores_es = zip(*frequencia_letras["espanhol"].most_common(5))
+
+    x = np.arange(len(rotulos_es))
+
+    width = 0.2
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width / 2, valores_pt, width, label="Português (Br)")
+    rects2 = ax.bar(x + width / 2, valores_es, width, label="Espanhol")
+    ax.set_ylabel("frequência das letras")
+    ax.set_title("Letras mais frequêntes em Gene Egoísta")
+    ax.set_xticks(x, rotulos_pt)
+    # ax.set_xticks(x, rotulos_es)
+    ax.legend()
+
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+
+    fig.tight_layout()
+
+    plt.show()
 
 
 if __name__ == "__main__":

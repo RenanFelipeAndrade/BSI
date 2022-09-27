70d507f1d4428d070869f6b7e63b28138321aa4f
Renan Andrade <Fel9.renan02@gmail.com>
2022-09-27 09:57:33 -0300

    Alterado palavras para json e seleção de tema

--- Python/Poetry/Embaralha palavra/index.py
+++ Python/Poetry/Embaralha palavra/index.py
@@ -3,9 +3,10 @@ from frase_motivacional import frase_motivacional
 from embaralha_palavra import embaralha_palavra
 
 
-def main(tema):
+def main(tema, dificuldade_selecionada):
     tentativa = 0
-    palavra_sorteada, dificuldade = sorteia_palavra(tema)
+    palavra_sorteada, dificuldade = sorteia_palavra(
+        tema, dificuldade_selecionada)
     palavra_embaralhada = embaralha_palavra(palavra_sorteada)
 
     while True:
@@ -46,9 +47,22 @@ Escolha um dos temas a seguir (digite o número do tema):
 >> """
             )
         )
-        if tema > 4 or tema < 1:
-            print("--------- Escolha um tema válido! ---------")
+
+        dificuldade = int(
+            input(
+                """
+Escolha uma das dificuldades a seguir (digite o número do dificuldade):
+    1 = fácil
+    2 = médio
+    3 = difícil
+    
+>> """
+            )
+        )
+
+        if tema > 4 or tema < 1 or dificuldade < 1 or dificuldade > 3:
+            print("--------- Escolha um tema e dificuldades válidas! ---------")
         else:
             break
 
-    main(tema)
+    main(tema, dificuldade)
--- Python/Poetry/Embaralha palavra/sorteia_palavra.py
+++ Python/Poetry/Embaralha palavra/sorteia_palavra.py
@@ -1,153 +1,21 @@
+import json
+from pathlib import Path
 from random import randint
 
 
-def sorteia_palavra(tema):
-    cidades = {
-        "f": [
-            "lages",
-            "criciúma",
-            "urubici",
-            "macaé",
-            "natal",
-            "cotia",
-            "majé",
-            "itú",
-            "sinóp",
-        ],
-        "m": [
-            "calcaia",
-            "taubaté",
-            "moçoró",
-            "sumaré",
-            "jaraguá",
-            "brusque",
-            "palhoça",
-            "curitiba",
-        ],
-        "d": [
-            "florianópolis",
-            "joinville",
-            "ituporanga",
-            "ressaquinha",
-            "petrópolis",
-            "uberlândia",
-            "ananindeua",
-            "carapicuíba",
-            "itaquaquecetuba",
-            "parnamirim",
-        ],
-    }
-
-    objetos = {
-        "f": [
-            "garfo",
-            "maiô",
-            "óculos",
-            "jeans",
-            "copo",
-            "harpa",
-            "funil",
-            "pote",
-            "faca",
-        ],
-        "m": [
-            "picareta",
-            "bigorna",
-            "pendulo",
-            "manomêtro",
-            "zabumba",
-            "notebook",
-            "pinico",
-            "cadeira",
-        ],
-        "d": [
-            "estetoscopio",
-            "zarabatana",
-            "quinticlave",
-            "sombrinha",
-            "nebulizador",
-            "namômetro",
-            "lantejoula",
-            "velocípede",
-        ],
-    }
-
-    paises = {
-        "f": [
-            "suíça",
-            "itália",
-            "chipre",
-            "suécia",
-            "rússia",
-            "china",
-            "chile",
-            "sudão",
-            "irã",
-        ],
-        "m": [
-            "monaco",
-            "austria",
-            "austrália",
-            "equador",
-            "noruega",
-            "albânia",
-            "romênia",
-            "georgia",
-            "turquia",
-            "iraque",
-        ],
-        "d": [
-            "eslovênia",
-            "somália",
-            "guatemala",
-            "nicarágua",
-            "filipinas",
-            "mogólia",
-            "cingapura",
-            "nigéria",
-            "camarões",
-            "uzbequistão",
-            "azerbaijão",
-        ],
-    }
-
-    verbos = {
-        "f": [
-            "falar",
-            "correr",
-            "pular",
-            "andar",
-            "sonhar",
-            "piscar",
-            "nadar",
-            "sentar",
-        ],
-        "m": [
-            "hibernar",
-            "trafegar",
-            "manipular",
-            "evacuar",
-            "capturar",
-            "caminhar",
-            "desfilar",
-            "reclamar",
-        ],
-        "d": [
-            "desfibrilar",
-            "tergivisar",
-            "perambular",
-            "interpelar",
-            "radicalizar",
-            "auscutar",
-            "impermebealizar",
-            "diagnosticar",
-        ],
-    }
+def sorteia_palavra(tema, dificuldade):
+    BASE_DIR = Path(__file__).resolve().parent
+    with open(f"{BASE_DIR}/palavras.json") as arquivo:
+        json_data = json.load(arquivo)
+    cidades = json_data["cidades"]
+    objetos = json_data["objetos"]
+    paises = json_data["paises"]
+    verbos = json_data["verbos"]
 
     temas = {1: cidades, 2: objetos, 3: paises, 4: verbos}
 
     dificuldades = ["fácil", "médio", "difícil"]
-    dificuldade_sorteada = dificuldades[randint(0, len(dificuldades) - 1)]
+    dificuldade_sorteada = dificuldades[dificuldade-1]
 
     lista_palavras = temas[tema][dificuldade_sorteada[0]]
 
